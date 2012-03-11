615d72e321b5a9d39f7631030bc2f84a15df9434
Hazem Khaled <hazem.khaled@gmail.com>
2012-03-11 20:14:16 +0200

    empty project

---
+++ Resources/app.js
@@ -0,0 +1,24 @@
+var win = Titanium.UI.createWindow({
+	backgroundColor : '#fff'
+});
+
+var combobox = require('libs/combobox').createCombobox({
+	height : 120,
+	barColor : '#F8001D',
+	data : []
+});
+win.add(combobox);
+
+var countryBtn = Ti.UI.createButton({
+	title : 'Select Country',
+	width : '150dp',
+	height : '40dp'
+});
+
+win.add(countryBtn);
+
+countryBtn.addEventListener('click', function () {
+	combobox.fireEvent('showMe');
+});
+
+win.open();
